783e813f5a532ab7a919e147ce14476f42239228
LAPTOPPT <LAPTOPPT@145.44.52.21>
2014-04-17 16:26:41 +0200

    Satan is cool!2

--- ISASP/src/isasp/ISASP.java
+++ ISASP/src/isasp/ISASP.java
@@ -16,8 +16,8 @@ public class ISASP {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       System.out.println("ME THE RULER OF THEM ALL!");
-      
+       System.out.println("Peter THE RULER OF THEM ALL!");
+       
     }
     
 }
