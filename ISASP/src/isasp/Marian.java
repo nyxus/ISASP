2206a00a9263876099580e6c3a1940c5c3072988
nyxus <gercoversloot@gmail.com>
2014-05-01 22:53:04 +0200

    Marian v1.0 Start Read problem

--- ISASP/src/isasp/Marian.java
+++ ISASP/src/isasp/Marian.java
@@ -5,11 +5,52 @@
  */
 
 package isasp;
-
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 /**
  *
  * @author Gerco
  */
 public class Marian {
+    private ArrayList<Chromosome> population;
+    private Block floor = new Block(0, 1, 1, 0, 0);
+    private Integer[][] matrix;
+    
+    
+    public void ReadProblem(String file){      
+        try {
+            //Open bestand op de opgegeven locatie
+            FileInputStream fstream = new FileInputStream(file);
+
+            // Get the object of DataInputStream
+            DataInputStream in = new DataInputStream(fstream);
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            String strLine;
+            //Lees bestand per regel
+            while ((strLine = br.readLine()) != null) {
+                //convertToMatrix(strLine, 1);
+                System.out.println(strLine);
+            }
+            //Close the input stream
+            in.close();
+        } catch (Exception e) {//Catch exception if any
+            System.err.println("Error: " + e.getMessage());
+        }   
+    }
+/*
+    void convertToMatrix(String string, int Row) {
+        //De te splitten string
+        String str = string;
+        String[] temp;
+
+        //Symbool waarmee hij hem afkapt
+        temp = str.split(" ");
+
+        //voeg substrings toe aan array        
+    }
+    */
     
 }
