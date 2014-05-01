1a2bc9667e646eb061b2b1ece103d5f7608390d2
LAPTOPPT <LAPTOPPT@LAPTOPPT-PC>
2014-05-01 16:30:14 +0200

    Block

--- ISASP/src/isasp/Block.java
+++ ISASP/src/isasp/Block.java
@@ -6,10 +6,30 @@
 
 package isasp;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Gerco
  */
 public class Block {
+    public int ID;
+    public int MinX;
+    public int MaxX;
+    public int MinY;
+    public int MaxY;
+    public ArrayList<Block> Parents;
+    public ArrayList<Block> Siblings;
     
+    public void AddParent(Block block){
+        this.Parents.add(block);
+    }    
+    
+    public void AddSibling(Block block){
+        this.Parents.add(block);
+    }    
+    
+    public void PrintBlock(String separator){
+        System.out.println(ID + separator + MinX + separator + MaxX + separator + MinY + separator + MaxY + separator + Parents.toString() + separator + Siblings.toString());
+    }
 }
