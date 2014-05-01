6e01060daf53428ca344e96091e3800c243cc7fd
nyxus <gercoversloot@gmail.com>
2014-05-01 17:22:37 +0200

    Chromosome v1.3 PrintChromosome made

--- ISASP/src/isasp/Chromosome.java
+++ ISASP/src/isasp/Chromosome.java
@@ -11,22 +11,36 @@ import java.util.ArrayList;
  *
  * @author Gerco
  */
-public class Chromosome <B> {
-    private ArrayList<B> Sequence = new ArrayList<B>();
+public class Chromosome {
+    private ArrayList<Block> Sequence = new ArrayList<Block>();
     private int ID;
     
-    public void AddBlockToSequence( B newBlock){
+    public void AddBlockToSequence( Block newBlock){
        this.Sequence.add(newBlock);
     }  
     
-    public ArrayList<B> GetSelection(int Min,int Max){
-        ArrayList<B> selection = new ArrayList<B>();
+    public ArrayList<Block> GetSelection(int Min,int Max){
+        ArrayList<Block> selection = new ArrayList<Block>();
         for (int i = Min; i < Max ; i++) {
             selection.get(i); 
         }
         return selection;
     }
     
+    public void PrintChromosome(){
+        this.PrintChromosome(",");
+    }
     
-    
+    public String PrintChromosome(String devider){
+        String output = new String(); 
+
+        int size = this.Sequence.size();
+        for (int i = 0; i < Sequence.size(); i++) { 
+            output += this.Sequence.get(i).getID();
+            if (--size != 0) {
+               output += devider + " ";
+            }
+        }
+        return output; 
+    } 
 }
